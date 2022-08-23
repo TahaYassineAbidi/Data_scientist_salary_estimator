52aba4400cb9f9e2cf79bf1e9daa40bcfb060a79
TahaYassineAbidi <abiditahayassine@gmail.com>
2022-08-23 14:45:52 +0100

    first commit

---
+++ data_collection.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import glassdoor_scraper as gs 
+import pandas as pd 
+
+path = "./chromedriver"
+
+df = gs.get_jobs('data scientist',1000, False, path, 15)
+
+df.to_csv('glassdoor_jobs.csv', index = False)
